0d51dde0553da25d69afe6deb8d8486f8e551f0c
igndukwe <ifeanyigloryndukwe@gmail.com>
2020-05-23 14:31:38 +1200

    Creating a project

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ndugl892/OneDrive - University of Otago/PhD Glory Ndukwe - Documents/MyPython2/MyDjango/project4/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
