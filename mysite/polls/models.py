2f6949af1a59d4f1b6f62a86928a9eadd7a04751
igndukwe <ifeanyigloryndukwe@gmail.com>
2020-05-29 14:09:20 +1200

    Testing

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,7 +12,12 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        # subtract oneday from now and
+        # check if that us less than or equals to published date
+        # and also check if published date is less than now
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,67 +1,58 @@
-# from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-from django.http import HttpResponse
-from django.template import loader
+from django.utils import timezone
 
-from .models import Question
-
-# Create your views here.
-# def index(request):
-# @Anyi - ve in '-pub_date' is in the reverse order
-# so that the latest publication dates show up first
-# latest_question_list = Question.objects.order_by("-pub_date")[:5]
-# for each question, give us its text
-# and saperate each word by commas
-# output = ", ".join([q.question_text for q in latest_question_list])
-# return HttpResponse(output)
+from .models import Choice, Question
 
 
-# def index(request):
-# @Anyi: get top 5 questions
-##latest_question_list = Question.objects.order_by("-pub_date")[:5]
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    # Anyi: this is the default context_object_name for ListViews
+    # context_object_name = "question_list"
+    # Anyi: override the default context_object_name
+    # from "question_list" to "latest_question_list"
+    context_object_name = "latest_question_list"
 
-# @Anyi: the path to the html template
-##template = loader.get_template("polls/index.html")
-
-# @Anyi: this context will be passed to the html template
-##context = {
-##    "latest_question_list": latest_question_list,
-##}
-
-# @Anyi: pass this context to the html template
-##return HttpResponse(template.render(context, request))
-
-
-def index(request):
-    # @Anyi: get top 5 questions
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-
-    return render(
-        request, "polls/index.html", {"latest_question_list": latest_question_list}
-    )
+    def get_queryset(self):
+        """Return the last five published questions."""
+        # -pub_date prints articles will be in revered order so that recent articles appear first
+        # return Question.objects.order_by("-pub_date")[:5]
+        # pub_date__lte: publication date is less than or equals to
+        return Question.objects.filter(pub_date__lte=timezone).order_by("-pub_date")[:5]
 
 
-# def detail(request, question_id):
-#   return HttpResponse("You're looking at question %s." % question_id)
-
-##def detail(request, question_id):
-##    try:
-##       question = Question.objects.get(pk=question_id)
-##    except Question.DoesNotExist:
-##       raise Http404("Question does not exist")
-##    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    # Anyi: this is the default context_object_name for DetailView
+    # context_object_name = "question"
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+    # Anyi: this is the default context_object_name for DetailView
+    # context_object_name = "question"
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "You didn't select a choice.",},
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
